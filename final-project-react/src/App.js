30edc4f6f33e148b2235ca5443f429602baab948
Josh Hepworth <jrhepworth12@gmail.com>
2018-11-28 14:14:05 -0600

    setting up basic login and register

--- final-project-react/src/App.js
+++ final-project-react/src/App.js
@@ -3,10 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+      super();
+      this.state = {
+          loggedIn: false,
+          username: ''
+      }
+  }
+
+  LogIn = (username) => {
+    this.setState({
+      loggedIn: true,
+      username: username
+    })
+  }
+
+  Logout = (username) => {
+    this.setState({
+      loggedIn: false,
+      username: username
+    })
+  }
   render() {
     return (
       <div className="App">
-
+      { this.state.loggedIn ? <MainContainer Logout={this.Logout}/> : <LoginRegister LogIn={this.LogIn}/> }
       </div>
     );
   }
---
+++ final-project-react/src/LoginRegister/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class LoginRegister extends Component {
+	constructor(){
+	    super();
+	    this.state = {
+	        
+	    }
+	}
+    render(){
+        return(
+            
+        )
+    }
+}
+export default LoginRegister;
